2bd2952a13aa65dfe9d32f7ac9c6ae6edf3b9106
chris wang <wangguanzzz@126.com>
2022-01-31 19:11:34 +0800

    add credentials

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:public_key],
+      secret_token: Rails.application.credentials.iex_client[:secret_key],
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+    client.price ticker_symbol
+  end
 end
